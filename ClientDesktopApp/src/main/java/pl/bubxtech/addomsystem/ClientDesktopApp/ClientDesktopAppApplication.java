65c21ab11d1ade5a23c2c43dd7a106c435162446
Jakub Walczak <Jakub.Walczak@pentacomp.pl>
2019-04-29 15:10:25 +0200

    add Client App Project

---
+++ ClientDesktopApp/src/main/java/pl/bubxtech/addomsystem/ClientDesktopApp/ClientDesktopAppApplication.java
@@ -0,0 +1,13 @@
+package pl.bubxtech.addomsystem.ClientDesktopApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientDesktopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientDesktopAppApplication.class, args);
+	}
+
+}
