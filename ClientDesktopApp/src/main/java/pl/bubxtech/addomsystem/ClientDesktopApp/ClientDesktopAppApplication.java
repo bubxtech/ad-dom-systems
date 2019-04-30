2d0d4fb90244675beaabff9c0b8977d2b6f7eaf3
Jakub Walczak <Jakub.Walczak@pentacomp.pl>
2019-04-30 15:39:07 +0200

    add first view

--- ClientDesktopApp/src/main/java/pl/bubxtech/addomsystem/ClientDesktopApp/ClientDesktopAppApplication.java
+++ ClientDesktopApp/src/main/java/pl/bubxtech/addomsystem/ClientDesktopApp/ClientDesktopAppApplication.java
@@ -1,13 +1,46 @@
 package pl.bubxtech.addomsystem.ClientDesktopApp;
 
-import org.springframework.boot.SpringApplication;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class ClientDesktopAppApplication {
+public class ClientDesktopAppApplication extends Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientDesktopAppApplication.class, args);
-	}
+    ConfigurableApplicationContext springContext;
+    Parent rootNode;
+    FXMLLoader fxmlLoader;
 
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void init() throws Exception {
+        springContext = new SpringApplicationBuilder(ClientDesktopAppApplication.class)
+                .run(getParameters().getRaw().toArray(new String[0]));
+
+        fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        fxmlLoader.setControllerFactory(springContext::getBean);
+
+        rootNode = fxmlLoader.load();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(rootNode));
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        springContext.stop();
+    }
 }
